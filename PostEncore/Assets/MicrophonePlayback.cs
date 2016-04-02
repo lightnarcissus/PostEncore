77c8509fec35f8a38db0fbc3d68a75a4a31cd20a
Ansh <lightnarcissus@gmail.com>
2016-04-02 17:10:45 -0400

    Fixed animation issues and no Exit

---
+++ PostEncore/Assets/MicrophonePlayback.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MicrophonePlayback : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        AudioSource aud = GetComponent<AudioSource>();
+        aud.clip = Microphone.Start("Built-in Microphone", true, 10, 44100);
+        aud.Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       
+
+    }
+}
